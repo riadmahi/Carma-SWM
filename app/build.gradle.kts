a61389cffec894bdaa1fe4d3e3005dc34b55f80b
Riad MAHI <riadmahi@eduroam-084130.grenet.fr>
2023-09-20 12:05:13 +0200

    fix crash on create user preferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.moment.studywithme"
         minSdk = 27
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
